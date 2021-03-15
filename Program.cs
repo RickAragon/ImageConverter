c55040133c615ab10f16e5a16d832bb6b09e6c95
Rick Aragon <ttwrick@gmail.com>
2021-03-15 11:30:28 -0600

    Added a try catch block to the conversion process for it to be more verbose. Changed the project type to dotnet 5. Updated the ImageMagick library. Fixed an error with PNG files

--- Program.cs
+++ Program.cs
@@ -107,49 +107,56 @@ namespace ConversorDeImagenes
             foreach(string path in paths)
             {
                 FileInfo fileInfo = new FileInfo(path);
-                using(MagickImage image = new MagickImage(path))
+                try
                 {
-                    image.Strip();
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
-                    image.Interlace = Interlace.Plane;
-                    image.Alpha(AlphaOption.Deactivate);
-                    image.FilterType = FilterType.Undefined;
-                    image.Trim();
-                    image.ColorType = ColorType.Palette;
-                    image.ColorSpace = ColorSpace.RGB;
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
-                    image.GaussianBlur(0.05, 0.1);
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
-                    image.Quality = 85;
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
-                    if(resFlag)
-                        image.Resize(new MagickGeometry(resPipe[0], resPipe[1]));
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
-                    image.Write($"{dirPath}\\{fileInfo.Name}");
-                    if(basear64)
+                    using(MagickImage image = new MagickImage(path))
                     {
-                        string codigo = image.ToBase64();
-                        string extension = fileInfo.Extension.Substring(1);
-                        if(posiblesTipos.ContainsKey(extension))
+                        image.Strip();
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
+                        image.Interlace = Interlace.Plane;
+                        image.Alpha(AlphaOption.Deactivate);
+                        image.FilterType = FilterType.Undefined;
+                        image.Trim();
+                        image.ColorType = ColorType.Optimize;
+                        image.ColorSpace = ColorSpace.RGB;
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
+                        image.GaussianBlur(0.05, 0.1);
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
+                        image.Quality = 85;
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
+                        if(resFlag)
+                            image.Resize(new MagickGeometry(resPipe[0], resPipe[1]));
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
+                        image.Write($"{dirPath}\\{fileInfo.Name}");
+                        if(basear64)
                         {
-                            codigo = $"data:image/{posiblesTipos[extension]};base64,{codigo}";
+                            string codigo = image.ToBase64();
+                            string extension = fileInfo.Extension.Substring(1);
+                            if(posiblesTipos.ContainsKey(extension))
+                            {
+                                codigo = $"data:image/{posiblesTipos[extension]};base64,{codigo}";
+                            }
+                            else
+                            {
+                                codigo = $"data:image/{extension};base64,{codigo}";
+                            }
+                            using(TextWriter tw = new StreamWriter(new FileStream($"{dirPath}\\{fileInfo.Name}.txt", FileMode.Append)))
+                            {
+                                tw.Write(codigo);
+                            }
                         }
-                        else
-                        {
-                            codigo = $"data:image/{extension};base64,{codigo}";
-                        }
-                        using(TextWriter tw = new StreamWriter(new FileStream($"{dirPath}\\{fileInfo.Name}.txt", FileMode.Append)))
-                        {
-                            tw.Write(codigo);
-                        }
+                        percentage += microInterval;
+                        WritePercentage(Math.Round(percentage));
                     }
-                    percentage += microInterval;
-                    WritePercentage(Math.Round(percentage));
+                } 
+                catch(Exception ex)
+                {
+                    WriteError($"Error!!! M: {ex}");
                 }
             }
             Console.WriteLine();
